aad5af597334a574357b45d204c24574e37976a6
Florian Briand <Nabellaleen@users.noreply.github.com>
2021-03-31 11:50:52 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const PrettierPlugin = require("prettier-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const getPackageJson = require('./scripts/getPackageJson');
+
+const {
+  version,
+  name,
+  license,
+  repository,
+  author,
+} = getPackageJson('version', 'name', 'license', 'repository', 'author');
+
+const banner = `
+  ${name} v${version}
+  ${repository.url}
+
+  Copyright (c) ${author.replace(/ *\<[^)]*\> */g, " ")} and project contributors.
+
+  This source code is licensed under the ${license} license found in the
+  LICENSE file in the root directory of this source tree.
+`;
+
+module.exports = {
+  mode: "production",
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+    library: 'MyLibrary',
+    libraryTarget: 'umd',
+    clean: true
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      extractComments: false
+    })],
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    host: "localhost",
+    static: path.join(__dirname, 'demo'),
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+        use: ['url-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new PrettierPlugin(),
+    new webpack.BannerPlugin(banner)
+  ]
+};
